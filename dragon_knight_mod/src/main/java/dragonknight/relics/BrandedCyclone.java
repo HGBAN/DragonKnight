7cad84dddb358fce0c301d1a6165503951566c26
HGBA <19857811731@163.com>
2023-11-20 20:37:24 +0800

    增加新遗物 烙印的气旋

---
+++ dragon_knight_mod/src/main/java/dragonknight/relics/BrandedCyclone.java
@@ -0,0 +1,52 @@
+package dragonknight.relics;
+
+import static dragonknight.DragonKnightMod.makeID;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import dragonknight.character.DragonPrince;
+
+public class BrandedCyclone extends BaseRelic {
+    public static final String ID = makeID("BrandedCyclone");
+
+    public boolean isUsed = false;
+
+    public BrandedCyclone() {
+        super(ID, "BrandedCyclone", RelicTier.COMMON, LandingSound.MAGICAL);
+        pool = DragonPrince.Enums.CARD_COLOR;
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+    @Override
+    public void onUseCard(AbstractCard targetCard, UseCardAction useCardAction) {
+        if (!isUsed) {
+            addToBot(new DrawCardAction(1));
+            this.flash();
+            this.grayscale = true;
+            this.isUsed = true;
+        }
+    }
+
+    @Override
+    public void atBattleStart() {
+        this.grayscale = false;
+        this.isUsed = false;
+    }
+
+    @Override
+    public void atTurnStart() {
+        this.grayscale = false;
+        this.isUsed = false;
+    }
+
+    @Override
+    public void onVictory() {
+        this.grayscale = false;
+        this.isUsed = false;
+    }
+}
