7734263205aaa05798bda0b2e9f4dcf33c70ae3b
HGBA <19857811731@163.com>
2023-11-21 13:30:14 +0800

    增加新卡牌审判

---
+++ dragon_knight_mod/src/main/java/dragonknight/cards/common/Judgement.java
@@ -0,0 +1,54 @@
+package dragonknight.cards.common;
+
+import static dragonknight.DragonKnightMod.imagePath;
+import static dragonknight.DragonKnightMod.makeID;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import basemod.abstracts.CustomCard;
+import dragonknight.character.DragonPrince;
+
+public class Judgement extends CustomCard {
+    public static final String ID = makeID("Judgement");
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = cardStrings.NAME;
+    private static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    private static final int COST = 2;
+    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+
+    public Judgement() {
+        super(ID, NAME, imagePath("cards/attack/default.png"), COST, DESCRIPTION, TYPE, DragonPrince.Enums.CARD_COLOR,
+                RARITY,
+                TARGET);
+        this.baseDamage = 14;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(4);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn),
+                AttackEffect.FIRE));
+    }
+
+    @Override
+    public void triggerOnExhaust() {
+        addToBot(new MakeTempCardInDrawPileAction(this.makeCopy(), 1, true, true));
+    }
+
+}
