28fa74841080686dc84188a6ad8a97cf5d2fbf9e
HGBA <19857811731@163.com>
2023-12-10 22:48:52 +0800

    修复bug

--- dragon_knight_mod/src/main/java/dragonknight/patch/BossRelicSelectPatch.java
+++ dragon_knight_mod/src/main/java/dragonknight/patch/BossRelicSelectPatch.java
@@ -3,9 +3,11 @@ package dragonknight.patch;
 import static dragonknight.DragonKnightMod.makeID;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.screens.select.BossRelicSelectScreen;
+import com.megacrit.cardcrawl.vfx.FloatyEffect;
 
 @SpirePatch(clz = BossRelicSelectScreen.class, method = "relicObtainLogic")
 public class BossRelicSelectPatch {
@@ -14,5 +16,18 @@ public class BossRelicSelectPatch {
             r.instantObtain(AbstractDungeon.player, 0, true);
             (AbstractDungeon.getCurrRoom()).rewardPopOutTimer = 0.25F;
         }
+    }
+}
+
+@SpirePatch(clz = AbstractRelic.class, method = "bossObtainLogic")
+class BossObtainPatch {
+    public static SpireReturn<Void> Prefix(AbstractRelic _instance,FloatyEffect ___f_effect) {
+        if (_instance.relicId.equals(makeID("AbyssDragon"))) {
+            _instance.isObtained = true;
+            ___f_effect.x = 0.0F;
+            ___f_effect.y = 0.0F;
+            return SpireReturn.Return();
+        }
+        return SpireReturn.Continue();
     }
 }
